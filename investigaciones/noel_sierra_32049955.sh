8e8d864ec78cc5cee3e095d9d7f9bacc6a3cf756
noeldavidsierra-eng <noeldavidsierra@gmail.com>
2026-06-29 10:56:45 -0400

    feat: Personalizar archivo de investigacion con datos personales

--- investigaciones/noel_sierra_32049955.sh
+++ investigaciones/noel_sierra_32049955.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # =========================================================================
 # EVALUACIÓN: GIT CON BASH - SISTEMAS OPERATIVOS
-# ESTUDIANTE: [ESTUDIANTE]
-# CÉDULA: [CEDULA]
-# CORREO: [CORREO]
-# TEMA: [TEMA]
+# ESTUDIANTE:Noel Sierra
+# CÉDULA: 32049955
+# CORREO: Noeldavidsierra@gmail.com
+# TEMA: Fundamentos del Hardware (CPU, RAM, discos y perifericos) 
 # =========================================================================
 
