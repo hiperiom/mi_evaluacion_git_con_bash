3c937f8fe62583c0555ad486f2b3d372f1bac35d
noeldavidsierra-eng <noeldavidsierra@gmail.com>
2026-06-29 10:52:14 -0400

    feat: crear archivo .sh base a partir de plantilla

---
+++ investigaciones/noel_sierra_32049955.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# =========================================================================
+# EVALUACIÓN: GIT CON BASH - SISTEMAS OPERATIVOS
+# ESTUDIANTE: [ESTUDIANTE]
+# CÉDULA: [CEDULA]
+# CORREO: [CORREO]
+# TEMA: [TEMA]
+# =========================================================================
+
